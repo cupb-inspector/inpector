6b7a6f75b4803ecd6a7330f66858aaf41f656aa0
aohanhongzhi <aohanhongzhi@qq.com>
2019-09-15 23:07:54 +0800

    解决订单问题

--- admin/src/main/java/cn/hxy/inspect/admin/service/OrderService.java
+++ admin/src/main/java/cn/hxy/inspect/admin/service/OrderService.java
@@ -1,8 +1,10 @@
 package cn.hxy.inspect.admin.service;
 
+import cn.hxy.inspect.dao.GetOrderStatusWithList;
 import cn.hxy.inspect.dao.OrdersDao;
 import cn.hxy.inspect.entity.Orders;
 import cn.hxy.inspect.entity.inspector.Inspector;
+import cn.hxy.inspect.util.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -17,163 +20,187 @@ import java.util.List;
 @Service
 public class OrderService {
 
-	private final static Logger logger = LoggerFactory.getLogger(OrderService.class);
+    private final static Logger logger = LoggerFactory.getLogger(OrderService.class);
 
-	// 插入订单
-	public boolean insert(Orders order) {
-		OrdersDao orderDao = new OrdersDao();
-		try {
-			int flag = orderDao.insert(order);
-			if (flag == 1) {
-				return true;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    // 插入订单
+    public boolean insert(Orders order) {
+        OrdersDao orderDao = new OrdersDao();
+        try {
+            int flag = orderDao.insert(order);
+            if (flag == 1) {
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	public List<Orders> selectAllByTel(String tel) throws IOException {
-		OrdersDao ordersDao = new OrdersDao();
-		List<Orders> list = ordersDao.selectAllByTel(tel);
-		return list;
-	}
+    public List<Orders> selectAllByTel(String tel) throws IOException {
+        OrdersDao ordersDao = new OrdersDao();
+        List<Orders> list = ordersDao.selectAllByTel(tel);
+        return list;
+    }
 
-	public List<Orders> selectAll() throws IOException {
-		OrdersDao ordersDao = new OrdersDao();
-		List<Orders> list = ordersDao.selectAll();
-		return list;
-	}
-	
-	
-	
-	public List<Orders> findUserByQualtelAndStatus(Orders orders) throws IOException {
-		OrdersDao ordersDao = new OrdersDao();
-		List<Orders> list = ordersDao.selectAll();
-		return list;
-	}
+    public List<Orders> selectAll() throws IOException {
+        OrdersDao ordersDao = new OrdersDao();
+        List<Orders> list = ordersDao.selectAll();
+        return list;
+    }
 
-	public Orders selectOrderById(String ordersId) throws IOException {
-		OrdersDao ordersDao = new OrdersDao();
-		Orders list = ordersDao.selectAllById(ordersId);
-		return list;
-	}
-	
-	
-	
 
-	public  List<Orders> selectOrdersByStatus(int status) throws IOException {
-		logger.info("查询状态为"+status+"的订单");
-		OrdersDao ordersDao = new OrdersDao();
-		 List<Orders> list = ordersDao.selectOrdersByStatus(status);
-		return list;
-	}
-	
-	public  List<Orders> selectOrdersByDateAndStatus(HashMap<String, Object> status) throws IOException {
-		OrdersDao ordersDao = new OrdersDao();
-		 List<Orders> list = ordersDao.selectOrdersByDateAndStatus(status);
-		return list;
-	}
-	//查询今天和明天的订单
-	public  List<Orders> selectTodayTomorrowOrders() throws IOException {
-		Date date =new Date();
-		DateFormat df1 = DateFormat.getDateInstance();//日期格式，精确到日,这个会有一点问题,如果是2019-5-17就不会是2019-05-17
-		  SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");  
-		String today = formatter.format(date);
-		logger.info("今日:"+today);
-		HashMap<String, Object> status = new HashMap<>();
-		status.put("excedate", today);
-		status.put("status", "5");
-		status.put("start", 0);
-		status.put("size", 5);//查询最近5条
+    public List<Orders> findUserByQualtelAndStatus(Orders orders) throws IOException {
+        OrdersDao ordersDao = new OrdersDao();
+        List<Orders> list = ordersDao.selectAll();
+        return list;
+    }
 
-		OrdersDao ordersDao = new OrdersDao();
-		 List<Orders> list = ordersDao.selectOrdersByDateAndStatus(status);
-		return list;
-	}
-	
+    public Orders selectOrderById(String ordersId) throws IOException {
+        OrdersDao ordersDao = new OrdersDao();
+        Orders list = ordersDao.selectAllById(ordersId);
+        return list;
+    }
 
-	public  List<Orders> selectOrdersByStatusJudge(HashMap<String, Object> status) throws IOException {
-		OrdersDao ordersDao = new OrdersDao();
-		 List<Orders> list = ordersDao.selectOrdersByStatusJudge(status);
-		return list;
-	}
-	
-	
 
-	public boolean updateInspect(Orders order) {
-		OrdersDao ordersDao = new OrdersDao();
-		int flag = ordersDao.updateInspect(order);
-		if (flag == 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public List<Orders> selectOrdersByStatus(int status) throws IOException {
+        logger.info("查询状态为" + status + "的订单");
+        OrdersDao ordersDao = new OrdersDao();
+        List<Orders> list = ordersDao.selectOrdersByStatus(status);
+        return list;
+    }
 
-	public boolean updateInspector(Orders order) {
-		OrdersDao ordersDao = new OrdersDao();
-		int flag = ordersDao.updateInspector(order);
-		if (flag == 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public boolean assignOrders(Orders order) {
-		OrdersDao ordersDao = new OrdersDao();
-		int flag = ordersDao.updateInspector(order);
-		if (flag == 1) {
-			//新建线程发送邮件到质检员
-			new Thread(new SendEmailRunnable(order)).start();
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public List<Orders> selectOrdersByDateAndStatus(HashMap<String, Object> status) throws IOException {
+        OrdersDao ordersDao = new OrdersDao();
+        List<Orders> list = ordersDao.selectOrdersByDateAndStatus(status);
+        return list;
+    }
 
-	public void updateReport(Orders orders) {
-		// TODO Auto-generated method stub
-		
-	}
+    //查询今天和明天的订单
+    public List<Orders> selectTodayTomorrowOrders() throws IOException {
+        Date date = new Date();
+        DateFormat df1 = DateFormat.getDateInstance();//日期格式，精确到日,这个会有一点问题,如果是2019-5-17就不会是2019-05-17
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String today = formatter.format(date);
+        logger.info("今日:" + today);
+        HashMap<String, Object> status = new HashMap<>();
+        status.put("excedate", today);
+        status.put("status", "5");
+        status.put("start", 0);
+        status.put("size", 5);//查询最近5条
 
-	public List<Orders> findOrdersByRange(HashMap<String, Object> map) {
-		// TODO Auto-generated method stub
-		cn.hxy.inspect.dao.OrdersDao ordersDao = new cn.hxy.inspect.dao.OrdersDao();
-		List<Orders> list=null;
-		try {
-			list = ordersDao.findOrdersByRange(map);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return list;
-	}
+        OrdersDao ordersDao = new OrdersDao();
+        List<Orders> list = ordersDao.selectOrdersByDateAndStatus(status);
+        return list;
+    }
 
+
+    public List<Orders> selectOrdersByStatusJudge(HashMap<String, Object> status) throws IOException {
+        OrdersDao ordersDao = new OrdersDao();
+        List<Orders> list = ordersDao.selectOrdersByStatusJudge(status);
+        return list;
+    }
+
+    public List<Orders> selectUnfinishedOrders() throws IOException {
+        OrdersDao ordersDao = new OrdersDao();
+
+
+        // 将status放入list中
+        List<Integer> listParam = new ArrayList<>();
+        OrderService o = new OrderService();
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        // 获得一部分status
+        listParam.addAll(GetOrderStatusWithList.getStatusSublist(Configuration.BILL_SUBMITTED,
+                Configuration.BILL_REFUSED_BY_ADMIN));
+
+        listParam.addAll(GetOrderStatusWithList.getStatusSublist(Configuration.BILL_ASSIGNING_BY_ADMIN_UNPAID,
+                Configuration.BILL_REFUSED_BY_ADMIN_UNPAID));
+
+        listParam.addAll(GetOrderStatusWithList.getStatusSublist(Configuration.BILL_INSPECTOR_CONFIRM,
+                Configuration.BILL_REPORT_VERIFIED));
+
+        listParam.addAll(GetOrderStatusWithList.getStatusSublist(Configuration.BILL_REPORT_REFUSED_BY_ADMIN_UNPAID,
+                Configuration.BILL_REPORT_PASSED_BY_ADMIN_UNPAID));
+
+        map.put("list", listParam);
+        List<Orders> list = ordersDao.findOrdersByRange(map);
+
+        return list;
+    }
+
+
+    public boolean updateInspect(Orders order) {
+        OrdersDao ordersDao = new OrdersDao();
+        int flag = ordersDao.updateInspect(order);
+        if (flag == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean updateInspector(Orders order) {
+        OrdersDao ordersDao = new OrdersDao();
+        int flag = ordersDao.updateInspector(order);
+        if (flag == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean assignOrders(Orders order) {
+        OrdersDao ordersDao = new OrdersDao();
+        int flag = ordersDao.updateInspector(order);
+        if (flag == 1) {
+            //新建线程发送邮件到质检员
+            new Thread(new SendEmailRunnable(order)).start();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void updateReport(Orders orders) {
+
+
+    }
+
+    public List<Orders> findOrdersByRange(HashMap<String, Object> map) throws IOException {
+
+        cn.hxy.inspect.dao.OrdersDao ordersDao = new cn.hxy.inspect.dao.OrdersDao();
+        List<Orders> list = ordersDao.findOrdersByRange(map);
+        return list;
+    }
+
+    public List<Orders> selectFinishedOrders(HashMap<String, Object> map) throws IOException {
+        cn.hxy.inspect.dao.OrdersDao ordersDao = new cn.hxy.inspect.dao.OrdersDao();
+        List<Orders> list = ordersDao.findOrdersByRange(map);
+        return list;
+    }
 }
 
 
+class SendEmailRunnable implements Runnable {
 
-class SendEmailRunnable implements Runnable{
+    private final static Logger logger = LoggerFactory.getLogger(SendEmailRunnable.class);
+    Orders orders;
 
-	private final static Logger logger = LoggerFactory.getLogger(SendEmailRunnable.class);
-	Orders orders;
-	SendEmailRunnable(Orders orders){
-		this.orders=orders;
-	}
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		InspectorService inspectorService = new InspectorService();
-		Inspector inspector = inspectorService.findInspectorById(orders.getQualId());
-		if (inspector != null && !"null".equals(inspector)) {
-			// 发送邮件给质检员
-			MailService mailService = new MailService();
-			mailService.sendMailToInspector(inspector);
-		} else {
-			logger.error("该质检员不存在：" + orders.getQualId());
-		}
-	}
-	
+    SendEmailRunnable(Orders orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public void run() {
+
+        InspectorService inspectorService = new InspectorService();
+        Inspector inspector = inspectorService.findInspectorById(orders.getQualId());
+        if (inspector != null && !"null".equals(inspector)) {
+            // 发送邮件给质检员
+            MailService mailService = new MailService();
+            mailService.sendMailToInspector(inspector);
+        } else {
+            logger.error("该质检员不存在：" + orders.getQualId());
+        }
+    }
+
 }
--- admin/src/main/webapp/WEB-INF/views/orders/orders-checkings.jsp
+++ admin/src/main/webapp/WEB-INF/views/orders/orders-checkings.jsp
@@ -1,34 +1,13 @@
 <jsp:include page="/WEB-INF/views/commons.jsp" />
-<%@page import="hxy.inspec.admin.services.OrderService"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<%@page import="hxy.inspec.admin.po.Orders"%>
-<%@page import="java.util.List"%>
-<!doctype html>
-<%@page import="hxy.inspec.admin.po.AdminUser"%>
-<%
-	AdminUser user = (AdminUser) request.getSession().getAttribute("user");
-	if (user == null) {
-		//request.getRequestDispatcher("/lose").forward(request, response);
-		%>
-<script type="text/javascript">
-    window.top.location.href = 'login';
-</script>
-<% 
-	} else {
-	
-	}
-	OrderService orderService = new OrderService();
-	List<Orders> ls = orderService.selectTodayTomorrowOrders();
-%>
-
+         pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang="">
 <!--<![endif]-->
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,14 +24,6 @@
     <link rel="stylesheet" href="assets/css/lib/datatable/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-    <style>
-        html,
-        body {
-            margin: 0px;
-            width: 100%;
-            height: 100%;
-        }
-    </style>
 </head>
 <body>
     <div class="content" style="background:#f1f2f7;height: 100%">
@@ -67,7 +38,6 @@
                             <table id="bootstrap-data-table" class="table table-striped table-bordered">
                                 <thead>
                                     <tr>
-                                        <th class="serial">#</th>
                                         <th>委托方</th>
                                         <th>订单号</th>
                                         <th>验货方</th>
@@ -76,25 +46,15 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <%
-										if (ls!=null&&ls.size() != 0) {
-											for (int i = 0; i < ls.size(); i++) {
-												Orders o=ls.get(i);
-									%>
+                                <c:forEach items="${list}" var="order" varStatus="status">
                                     <tr>
-                                        <td><%=i+1 %></td>
-                                        <td><%=o.getCusId() %></td>
-                                        <td><%=o.getOrderid() %></td>
-                                        <td><%=o.getQualId() %></td>
-                                        <td><%=o.getExcedate() %></td>
-                                        <td><a href="details-orders2?id=<%=o.getOrderid() %>" target="myiframe"
-                                                style="color: blue">详情</a></td>
+                                        <td>${order.cusId }</td>
+                                        <td>${order.orderid}</td>
+                                        <td>${order.qualId}</td>
+                                        <td>${order.excedate}</td>
+                                        <td><a href="details3?id=${ order.orderid }">详情</a></td>
                                     </tr>
-                                    <%
-										}
-										}
-									%>
-
+                                </c:forEach>
                                 </tbody>
                             </table>
                         </div>
